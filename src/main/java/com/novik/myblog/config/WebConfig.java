1b4dc98b47bd89c58d3e05641079fc5ea6d8bcb2
sergey <noviks88@yandex.ru>
2025-03-21 21:25:50 +1000

    feat: add configuration

---
+++ src/main/java/com/novik/myblog/config/WebConfig.java
@@ -0,0 +1,13 @@
+package com.novik.myblog.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = {"com.novik.myblog"})
+@PropertySource("classpath:application.properties")
+public class WebConfig {
+}
--- src/main/java/com/novik/myblog/repository/TagRepositoryImpl.java
+++ src/main/java/com/novik/myblog/repository/TagRepositoryImpl.java
@@ -3,9 +3,15 @@ package com.novik.myblog.repository;
 import com.novik.myblog.model.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.HashSet;
 import java.util.Set;
 
 @RequiredArgsConstructor
@@ -20,13 +26,26 @@ public class TagRepositoryImpl implements TagRepository {
 
         KeyHolder keyHolder = new GeneratedKeyHolder();
 
-
-
-        return null;
+        jdbcTemplate.update(connection -> {
+            PreparedStatement ps = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1, tag.getTitle());
+            return ps;
+        }, keyHolder);
+        tag.setId(keyHolder.getKey().longValue());
+        return tag;
     }
 
     @Override
     public Set<Tag> findTagsByTitleIn(Set<String> titles) {
-        return Set.of();
+        String sql = "SELECT * FROM tags WHERE title IN (:titles)";
+
+        return new HashSet<>(jdbcTemplate.query(sql, this::mapRow, titles));
+    }
+
+    public Tag mapRow(ResultSet rs, int rowNum) throws SQLException {
+        return new Tag(
+                rs.getLong("id"),
+                rs.getString("title")
+        );
     }
 }
